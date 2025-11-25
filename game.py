d4ffdf196017b6bff65139330f7921b1c0ae550f
Minoru Sakakibara <qqgm7z69@gmail.com>
2025-11-25 21:59:01 +0900

    update20251125

--- game.py
+++ game.py
@@ -8,13 +8,14 @@ START_SCENE = 'start'
 PLAY_SCENE = 'play'
 
 class Sword:
-    def __init__(self, x, y):
+    def __init__(self, x, y, speed):
         self.x = x
         self.y = y
+        self.speed = speed
     
     def update(self):
         if self.y < SCREEN_HEIGHT:
-            self.y += 2
+            self.y += self.speed
     
     def draw(self):
         pyxel.blt(self.x, self.y, 0, 8, 0, 8, 16, pyxel.COLOR_BLACK)
@@ -32,9 +33,15 @@ class App:
     def reset_play_scene(self):
         self.player_x = SCREEN_WIDTH // 2
         self.player_y = SCREEN_HEIGHT * 4 // 5
+        self.player_vy = 0
         self.swords = []
         self.is_collision = False
         self.game_over_display_timer = GAME_OVER_DISPLAY_TIME
+        self.sword_speed_level = 1  # 剣の落下速度のレベル
+        self.initial_sword_speed = 2  # 剣の初期落下速度
+        self.sword_spawn_level = 1 # 剣の出現数を管理するレベル
+        self.initial_sword_interval = 15 # 剣の初期出現間隔
+        self.score = 0  # スコアを初期化
 
     def update_start_scene(self):
         if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
@@ -60,9 +67,38 @@ class App:
         elif pyxel.btn(pyxel.KEY_A) and self.player_x > - 4:
             self.player_x -= 1
         
+        # ジャンプ機能の追加
+        # 地面にいるときのみジャンプ可能
+        if pyxel.btnp(pyxel.KEY_SPACE) and self.player_y == SCREEN_HEIGHT * 4 // 5:
+            self.player_vy = -4  # ジャンプの初速度
+        
+        # プレイヤーのY座標を更新
+        self.player_y += self.player_vy
+        
+        # 重力
+        if self.player_y < SCREEN_HEIGHT * 4 // 5:
+            self.player_vy += 0.2  # 重力加速度
+        
+        # 地面との衝突判定
+        if self.player_y >= SCREEN_HEIGHT * 4 // 5:
+            self.player_y = SCREEN_HEIGHT * 4 // 5
+            self.player_vy = 0
+            
+         # 剣の落下速度の変更
+        if self.sword_speed_level < 3 and pyxel.frame_count % (10 * 60) == 0:
+            self.sword_speed_level += 1
+            
+        current_speed = self.initial_sword_speed * self.sword_speed_level
+        
+        # 剣の出現数の変更
+        if self.sword_spawn_level < 3 and pyxel.frame_count % (10 * 60) == 0:
+            self.sword_spawn_level += 1
+        
+        current_interval = self.initial_sword_interval // self.sword_spawn_level
+        
         # 剣を追加
-        if pyxel.frame_count % SWORD_INTERVAL == 0:
-            self.swords.append(Sword(pyxel.rndi(0, SCREEN_WIDTH - 8), 0))
+        if pyxel.frame_count % current_interval == 0:
+            self.swords.append(Sword(pyxel.rndi(0, SCREEN_WIDTH - 8), 0, current_speed))
         
         # 剣の落下
         for sword in self.swords.copy():
@@ -73,9 +109,11 @@ class App:
                 self.player_y <= sword.y <= self.player_y + 8):
                 self.is_collision = True
                 
-            # 画面外の件を削除
+            # 画面外の剣を削除し、スコアを更新
             if sword.y >= SCREEN_HEIGHT:
-                self.swords.remove(sword) 
+                self.swords.remove(sword)
+                if self.score < 9999:  # スコアが最大値に達していないかチェック
+                    self.score += 10
     
     def update(self):
         if pyxel.btnp(pyxel.KEY_ESCAPE):
@@ -93,6 +131,10 @@ class App:
     
     def draw_play_scene(self):
         pyxel.cls(pyxel.COLOR_DARK_BLUE)
+        
+        # スコアの表示
+        pyxel.text(5, 5, 'SCORE: ' + str(self.score), pyxel.COLOR_WHITE)
+        
         # 剣
         for sword in self.swords:
             sword.draw()
@@ -103,6 +145,9 @@ class App:
         if self.is_collision:
             pyxel.text(SCREEN_WIDTH // 2 - 15, SCREEN_HEIGHT // 2,
                        'GAME OVER', pyxel.COLOR_RED)
+            # ゲームオーバー時に最終スコアを表示
+            pyxel.text(SCREEN_WIDTH // 2 - 20, SCREEN_HEIGHT // 2 + 10,
+                       'FINAL SCORE: ' + str(self.score), pyxel.COLOR_RED)
    
     def draw(self):
         if self.current_scene == START_SCENE:
